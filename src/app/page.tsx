7f60691d7bf598f1c6b8310f5cca63ecbedf6501
muneebulhassanpak <muneebulhassan121@gmail.com>
2023-11-18 18:04:58 +0500

    Simple home page desiging and linking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="text-center py-16 flex justify-center items-center min-h-screen bg-black">
+      <Link
+        href="/portfolio"
+        className="px-8 py-3 bg-white text-black uppercase rounded-sm"
+      >
+        See Portfolio
+      </Link>
+    </section>
+  );
 };
 
 export default Home;
